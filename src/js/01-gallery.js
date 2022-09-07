c7de8291d52809e7f47dd5ee0b8b43c7afbcf7a9
MikeTribrat <mike.lapp.ua@gmail.com>
2022-09-07 13:59:40 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+console.log(galleryItems);
 
+const galleryEl = document.querySelector('.gallery'); 
+
+// 1. Создание и рендер разметки по массиву данных galleryItems
+
+const galleryMarkup = galleryItems.map(({ original, preview, description }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`)
+    .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+console.log(galleryEl);
+
+//  3. Инициализация библиотеки после того, как элементы галереи созданы и добавлены в `div.gallery`.
+
+const lightboxOptions = {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+};
+
+const lightboxGallery = new SimpleLightbox(".gallery a", lightboxOptions);
 console.log(galleryItems);
